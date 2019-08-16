921a770d67ef9be92beeed59c2b14aee410fd984
root <lv23ck@hotmail.com>
2019-08-16 16:33:28 +0800

    change datapath to cars98

--- dataset.py
+++ dataset.py
@@ -12,7 +12,7 @@ __all__ = ['CustomDataset']
 
 config = {
     # e.g. train/val/test set should be located in os.path.join(config['datapath'], 'train/val/test')
-    'datapath': 'DATA_PATH',
+    'datapath': '/home/chk/cars_stanford/cars_train_labelled',
 }
 
 
